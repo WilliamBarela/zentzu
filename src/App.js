5ac2fc66e38071be94b1299cf98fbcd0cccc2212
William Barela <william.barela.dev@gmail.com>
2020-09-15 22:19:46 -0500

    add initial Router boilerplate to App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import logo from './img/lauburu.svg';
 import './css/App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
 
 function App() {
   return (
-    <>
-      {/* <Header /> */}
-
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
